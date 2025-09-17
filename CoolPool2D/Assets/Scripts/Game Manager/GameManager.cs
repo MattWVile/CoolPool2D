1590bae4d60693b6ba785bf83037e634e831fe01
Matt Vile <matt.vile@yahoo.co.uk>
2025-09-17 20:58:40 +0100

    extracted ballsnapshot

--- CoolPool2D/Assets/Scripts/Game Manager/GameManager.cs
+++ CoolPool2D/Assets/Scripts/Game Manager/GameManager.cs
@@ -2,68 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using UnityEngine;
-
-[Serializable]
-public class BallSnapshot
-{
-    public int InstanceId;
-    public BallColour Colour;
-    public Vector2 Position;
-    public Vector2 Velocity;
-    public bool Active;
-}
-
-public class ShotRecorder
-{
-    private readonly List<BallSnapshot> _lastShotSnapshot = new List<BallSnapshot>();
-
-    public void SaveSnapshot(IEnumerable<GameObject> balls)
-    {
-        _lastShotSnapshot.Clear();
-        if (balls == null) return;
-
-        foreach (var go in balls)
-        {
-            if (go == null) continue;
-            var data = go.GetComponent<BallData>();
-            if (data == null) continue;
-            var det = go.GetComponent<DeterministicBall>();
-
-            var snap = new BallSnapshot
-            {
-                InstanceId = go.GetInstanceID(),
-                Colour = data.BallColour,
-                Position = (Vector2)go.transform.position,
-                Velocity = det != null ? det.velocity : Vector2.zero,
-                Active = det != null ? det.active : go.activeInHierarchy
-            };
-            _lastShotSnapshot.Add(snap);
-        }
-    }
-
-    public IReadOnlyList<BallSnapshot> GetLastSnapshot() => _lastShotSnapshot.AsReadOnly();
-
-    // Restore by setting existing objects (match by instance id). Useful if you never destroyed originals.
-    public void RestoreSnapshotToExistingObjects()
-    {
-        if (_lastShotSnapshot == null || _lastShotSnapshot.Count == 0) return;
-
-        var map = _lastShotSnapshot.ToDictionary(s => s.InstanceId);
-        foreach (var det in UnityEngine.Object.FindObjectsOfType<DeterministicBall>())
-        {
-            if (det == null) continue;
-            var go = det.gameObject;
-            if (map.TryGetValue(go.GetInstanceID(), out var snap))
-            {
-                go.transform.position = snap.Position;
-                det.velocity = snap.Velocity;
-                det.active = snap.Active;
-            }
-        }
-    }
-}
 
 public class GameManager : MonoBehaviour
 {
