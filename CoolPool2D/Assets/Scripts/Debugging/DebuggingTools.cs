89f3972330daa599b6d9e4e1a7414f8320d3e237
Matt Vile <matt.vile@yahoo.co.uk>
2025-10-16 17:30:09 +0100

    removed debug

--- CoolPool2D/Assets/Scripts/Debugging/DebuggingTools.cs
+++ CoolPool2D/Assets/Scripts/Debugging/DebuggingTools.cs
@@ -17,7 +17,6 @@ public class DebuggingTools : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log($"{GameManager.Instance.lastPottedBall} potted ball");
         HandleTimeControl();
         HandleGameTools();
         HandleDeleteAllBalls();
